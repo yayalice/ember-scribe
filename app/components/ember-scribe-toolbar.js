08aed8863bbf8796c7cfafd3d9b2aebc5c698587
Alice Li <alice@addepar.com>
2014-06-30 18:49:57 -0700

    working ember scribe component

--- app/components/ember-scribe-toolbar.js
+++ app/components/ember-scribe-toolbar.js
@@ -1,4 +1,12 @@
 export default Ember.Component.extend({
   layoutName: 'ember-scribe-toolbar',
-  scribeComponent: undefined
+
+  //Public interface
+
+  scribeComponent: undefined,
+  onScribeInitialization: Ember.observer(function() {
+    var scribe = this.get('scribeComponent.scribe'),
+        scribePluginToolbar = window.ScribeShim.require('scribe-plugin-toolbar');
+    scribe.use(scribePluginToolbar(this.$('.scribe-toolbar')[0]));
+  }, 'scribeComponent.scribe'),
 });
--- app/components/ember-scribe.js
+++ app/components/ember-scribe.js
@@ -1,19 +1,27 @@
 export default Ember.Component.extend({
   layoutName: 'ember-scribe',
 
+  // Public interface
+  initialText: 'Hello World!',
+  onContentChanged: Ember.K,  // TODO: fix this
+
   scribe: undefined,
   selectedFontName: 'Helvetica Neue',
   selectedFontSize: '2',
   selectedForeColor: 'rgb(0, 0, 0)',
   isToolbarVisible: true,
-  EDITOR_CLASS: 'text-editor',
-  PLACEHOLDER_TEXT: 'Click to edit',
 
   didInsertElement: function() {
     var Scribe = window.ScribeShim.require('scribe');
-    this.scribe = new Scribe(document.querySelector('.scribe'), {
+    this.scribe = new Scribe(this.$('.scribe')[0], {
       allowBlockElements: true
     });
-    this.scribe.on('content-changed', Ember.K);
+    this.scribe.setContent('<p>' + this.get('initialText') + '</p>');
+    document.querySelector('.scribe-html').textContent = this.scribe.getHTML();
+    this.scribe.on("content-changed", (function(_this) {
+      return function() {
+        document.querySelector(".scribe-html").textContent = _this.scribe.getHTML();
+      };
+    })(this));
   }
 });
